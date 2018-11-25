7337ba15ee9f411f300d497b98dab44bb8766bba
alex <aa26_ph@yahoo.com>
2018-11-25 19:54:12 +0800

    allow cors

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001;
 
+app.configure(function () {
+  app.use(allowCrossDomain);
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
