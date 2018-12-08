a29b8ff83554615b7920c903fb576c8f3b3a2acb
alex <aa26_ph@yahoo.com>
2018-12-08 19:30:10 +0800

    added logger.ts

--- dist/logger.js
+++ dist/logger.js
@@ -1,12 +1,13 @@
+"use strict";
 const { createLogger, format, transports } = require('winston');
 module.exports = createLogger({
-  level: 'info',
-  format: format.combine(
+    level: 'info',
+    format: format.combine(
     //format.splat(),
-    format.simple()
-  ),
-  transports: [
-    new transports.File({ filename: 'combined.log' }),
-	new transports.Console()
-  ]
+    format.simple()),
+    transports: [
+        new transports.File({ filename: 'combined.log' }),
+        new transports.Console()
+    ]
 });
+//# sourceMappingURL=logger.js.map
